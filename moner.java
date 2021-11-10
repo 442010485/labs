4b752d131bce5cf1a01184a0a17fb320c3206c46
442010485 <91111202+442010485@users.noreply.github.com>
2021-11-10 20:29:43 +0300

    Create moner.java

---
+++ moner.java
@@ -0,0 +1,11 @@
+public class moner{
+	public static void main(String[] args) {
+	
+	String name = "MonerALmalki";
+	
+	int var= (name.length()/2);
+
+    System.out.println(name.charAt(var));
+	
+	}
+}
