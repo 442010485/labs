c6a1d5b02e4f5a506ae5fbb51e171eb5b56c7176
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-21 00:02:52 +0300

    Create Ex.for

---
+++ Ex.for
@@ -0,0 +1,13 @@
+public class Ex{
+	public static void main(String[] args) {
+    for ( int x=100; x>=0 ; --x){
+        
+        
+         if ( x %2==0)
+         System.out.println(x+"even");
+    
+         else {
+             System.out.println(x+"odd");
+             
+             
+	}}}}
